eb65e99e3d769cef8fb6ba5e39e3f1aa739bb507
Yuhau Lin <lyuhau@gmail.com>
2024-09-13 13:53:32 -0400

    NeetCode150 38

--- neetcode150/38.remove_nth_node_from_end_of_list.py
+++ neetcode150/38.remove_nth_node_from_end_of_list.py
@@ -5,11 +5,24 @@ from neetcode150.util.list_node import ListNode
 
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        pass
+        nth = head
+        for i in range(n+1):
+            if not nth:
+                return head.next
+            nth = nth.next
+        curr = head
+        while nth:
+            curr = curr.next
+            nth = nth.next
+        curr.next = curr.next.next
+        return head
 
 
 if __name__ == '__main__':
     solution = Solution()
     head = ListNode.from_list([1, 2, 3, 4, 5])
     head = solution.removeNthFromEnd(head, 2)
+    print(head)
+    head = ListNode.from_list([1, 2, 3, 4, 5])
+    head = solution.removeNthFromEnd(head, 5)
     print(head)
